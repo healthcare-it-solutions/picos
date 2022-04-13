6432935a3e1e3e7c63484e803cb3bd2535d9a4ec
Alex Funkner <aleksej.funkner@hit-solutions.de>
2022-04-13 17:32:06 +0200

    idk

---
+++ lib/catalog_of_items.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CatalogOfItems extends StatelessWidget {
+  const CatalogOfItems({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'catalog_of_items.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: Map.fromIterable({ 'something', CatalogOfItems}),
     );
   }
 }
