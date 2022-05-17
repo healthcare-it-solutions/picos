9e92e9f20734deea582590f41f5eb04dd2eeef5f
SirDan <daniel@giemc.de>
2022-05-17 13:27:07 +0200

    Trailing Commas example

--- lib/models/medication.dart
+++ lib/models/medication.dart
@@ -3,12 +3,13 @@ import 'package:equatable/equatable.dart';
 /// Class with medication information.
 class Medication extends Equatable {
   /// Creates a medication object.
-  const Medication(
-      {required this.compound,
-      required this.morning,
-      required this.noon,
-      required this.evening,
-      required this.night});
+  const Medication({
+    required this.compound,
+    required this.morning,
+    required this.noon,
+    required this.evening,
+    required this.night,
+  });
 
   /// The amount to take in the morning.
   final double morning;
@@ -29,18 +30,20 @@ class Medication extends Equatable {
   static const List<String> selection = <String>['0', '1/2', '1', '2', '3'];
 
   /// Returns a copy of this medication with the given values updated.
-  Medication copyWith(
-      {String? compound,
-      double? morning,
-      double? noon,
-      double? evening,
-      double? night}) {
+  Medication copyWith({
+    String? compound,
+    double? morning,
+    double? noon,
+    double? evening,
+    double? night,
+  }) {
     return Medication(
-        compound: compound ?? this.compound,
-        morning: morning ?? this.morning,
-        noon: noon ?? this.noon,
-        evening: evening ?? this.evening,
-        night: night ?? this.night);
+      compound: compound ?? this.compound,
+      morning: morning ?? this.morning,
+      noon: noon ?? this.noon,
+      evening: evening ?? this.evening,
+      night: night ?? this.night,
+    );
   }
 
   @override
--- lib/screens/questionaire_screen/questionaire_screen.dart
+++ lib/screens/questionaire_screen/questionaire_screen.dart
@@ -32,12 +32,13 @@ class QuestionaireScreen extends StatelessWidget {
       verticalDirection: VerticalDirection.down,
       children: const <Widget>[
         QuestionaireCard(
-            question: 'What food do you like the most?',
-            answers: <int, String>{1: 'Pizza', 2: 'Sushi', 3: 'Curry'}),
+          question: 'What food do you like the most?',
+          answers: <int, String>{1: 'Pizza', 2: 'Sushi', 3: 'Curry'},
+        ),
         QuestionaireCard(
           question: 'Are you feeling well today?',
           answers: <int, String>{1: 'Feeling great! :)', 2: 'Feeling ok'},
-        )
+        ),
       ],
     );
   }
