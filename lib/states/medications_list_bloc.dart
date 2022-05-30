682b62ac2cfb1c72d747d927e238ef3b2ee94d05
mustafa_hits <mustafa.sezer@hit-solutions.de>
2022-05-30 13:23:51 +0200

    changes import for "bloc"

--- lib/states/medications_list_bloc.dart
+++ lib/states/medications_list_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:picos/repository/medications_repository.dart';
 
