53a6676b5c2e58c4131a2fa538e165ab285e19cf
Daniel Matus <daniel.matus@hit-solutions.de>
2022-07-20 08:54:01 +0200

    created list_card.dart

---
+++ lib/widgets/list_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListCard extends StatelessWidget {
+  const ListCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
