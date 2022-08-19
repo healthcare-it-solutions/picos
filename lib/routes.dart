d4eaf3893ce9871599ecd93acc13c17da67fae4e
Daniel Matus <daniel.matus@hit-solutions.de>
2022-08-19 12:36:50 +0200

    Fixing routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,9 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:picos/screens/add_family_member_screen.dart';
 import 'package:picos/screens/add_medication_screen/add_medication_screen.dart';
 import 'package:picos/screens/add_physician_screen.dart';
+import 'package:picos/screens/family_members_screen.dart';
 import 'package:picos/screens/my_medications_screen/my_medications_screen.dart';
+import 'package:picos/screens/physicians_screen.dart';
 
 ///This is a central place to manage all routes contained in the app.
+///Remember to put the "/" when using the routes.
 class Routes {
   ///Builds the routes.
   Routes(BuildContext ctx) {
@@ -13,6 +16,8 @@ class Routes {
       '/add-medication': (BuildContext ctx) => const AddMedicationScreen(),
       '/add-family-member': (BuildContext ctx) => const AddFamilyMemberScreen(),
       '/add-physician': (BuildContext ctx) => const AddPhysicianScreen(),
+      '/physicians': (BuildContext ctx) => const PhysiciansScreen(),
+      '/family-members': (BuildContext ctx) => const FamilyMembersScreen(),
     };
   }
 
