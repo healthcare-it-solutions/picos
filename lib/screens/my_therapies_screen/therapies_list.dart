988a7fae018ee491b6558d7f8bb6e817b838cd8b
Daniel Matus <daniel.matus@hit-solutions.de>
2022-09-14 11:49:17 +0200

    Adding therapy_list.dart

---
+++ lib/screens/my_therapies_screen/therapies_list.dart
@@ -0,0 +1,59 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:picos/state/medications_list_bloc.dart';
+import 'therapy_item.dart';
+
+/// A List with all therapies.
+class TherapiesList extends StatefulWidget {
+  /// Creates TherapiesList.
+  const TherapiesList({Key? key}) : super(key: key);
+
+  @override
+  State<TherapiesList> createState() => _TherapiesListState();
+}
+
+class _TherapiesListState extends State<TherapiesList> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MedicationsListBloc, MedicationsListState>(
+      builder: (BuildContext context, MedicationsListState state) {
+        if (state.medicationsList.isEmpty &&
+            state.status == MedicationsListStatus.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (state.status == MedicationsListStatus.failure) {
+          return const Center(
+            child: Text('Error'),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: state.medicationsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return TherapyItem(state.medicationsList[index]);
+          },
+        );
+      },
+    );
+  }
+}
