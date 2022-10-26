5d35498022debfbf8bfeb14b35a9d530263b918f
Daniel Matus <daniel.matus@hit-solutions.de>
2022-10-26 16:09:18 +0200

    Implementing simple TextFields in questionaire_screen.dart

--- lib/screens/questionaire_screen/questionaire_screen.dart
+++ lib/screens/questionaire_screen/questionaire_screen.dart
@@ -15,8 +15,18 @@
 *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'dart:core';
+
 import 'package:flutter/material.dart';
-import 'package:picos/screens/questionaire_screen/questionaire_card.dart';
+import 'package:picos/screens/questionaire_screen/widgets/cover.dart';
+import 'package:picos/screens/questionaire_screen/widgets/questionaire_card.dart';
+import 'package:picos/screens/questionaire_screen/widgets/text_field_card.dart';
+import 'package:picos/widgets/picos_body.dart';
+import 'package:picos/widgets/picos_screen_frame.dart';
+import 'package:picos/widgets/picos_text_field.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../widgets/picos_label.dart';
 
 /// This is the screen a user should see when prompted to provide some
 /// information about their health status.
@@ -24,34 +34,133 @@ class QuestionaireScreen extends StatelessWidget {
   /// QuestionaireScreen constructor
   const QuestionaireScreen({Key? key}) : super(key: key);
 
+  //Static Strings
+  static String? _myEntries;
+  static String? _vitalValues;
+  static String? _activityAndRest;
+  static String? _bodyAndMind;
+  static String? _medicationAndTherapy;
+  static String? _ready;
+  static String? _bodyWeight;
+  static String? _autoCalc;
+  static String? _heartFrequency;
+  static String? _bloodPressure;
+  static String? _bloodSugar;
+  static String? _possibleWalkDistance;
+  static String? _sleepDuration;
+  static String? _hrs;
+
+  static List<PicosBody>? _pageViews;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    if (_myEntries == null) {
+      _myEntries = AppLocalizations.of(context)!.myEntries;
+      _vitalValues = AppLocalizations.of(context)!.vitalValues;
+      _activityAndRest = AppLocalizations.of(context)!.activityAndRest;
+      _bodyAndMind = AppLocalizations.of(context)!.bodyAndMind;
+      _medicationAndTherapy =
+          AppLocalizations.of(context)!.medicationAndTherapy;
+      _bodyWeight = AppLocalizations.of(context)!.bodyWeight;
+      _autoCalc = AppLocalizations.of(context)!.autoCalc;
+      _heartFrequency = AppLocalizations.of(context)!.heartFrequency;
+      _bloodPressure = AppLocalizations.of(context)!.bloodPressure;
+      _bloodSugar = AppLocalizations.of(context)!.bloodSugar;
+      _possibleWalkDistance =
+          AppLocalizations.of(context)!.possibleWalkDistance;
+      _sleepDuration = AppLocalizations.of(context)!.sleepDuration;
+      _hrs = AppLocalizations.of(context)!.hrs;
+      _ready = AppLocalizations.of(context)!.questionnaireFinished;
+
+      _pageViews = <PicosBody>[
+        PicosBody(child: Cover(title: _vitalValues!)),
+        PicosBody(
+          child: TextFieldCard(
+            label: _bodyWeight!,
+            hint: 'kg',
+          ),
+        ),
+        PicosBody(
+          child: TextFieldCard(
+            label: 'BMI',
+            hint: 'kg/m² ${_autoCalc!}',
+          ),
+        ),
+        PicosBody(
+          child: TextFieldCard(
+            label: _heartFrequency!,
+            hint: 'bpm',
+          ),
+        ),
+        PicosBody(
+          child: QuestionaireCard(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                PicosLabel(label: _bloodPressure!),
+                const SizedBox(height: 15),
+                Row(
+                  children: const <Widget>[
+                    Expanded(
+                      child: PicosTextField(
+                        maxLength: 3,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 20, left: 10, right: 10),
+                      child: Text('/', style: TextStyle(fontSize: 20)),
+                    ),
+                    Expanded(
+                      child: PicosTextField(
+                        maxLength: 3,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        PicosBody(
+          child: TextFieldCard(
+            label: _bloodSugar!,
+            hint: 'mg/dL',
+          ),
+        ),
+        PicosBody(child: Cover(title: _activityAndRest!)),
+        PicosBody(
+          child: TextFieldCard(
+            label: _possibleWalkDistance!,
+            hint: 'Meter',
+          ),
+        ),
+        PicosBody(
+          child: TextFieldCard(
+            label: _sleepDuration!,
+            hint: _hrs!,
+          ),
+        ),
+        PicosBody(child: Text(_activityAndRest!)),
+        PicosBody(child: Cover(title: _bodyAndMind!)),
+        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(child: Cover(title: _medicationAndTherapy!)),
+        PicosBody(child: Text(_medicationAndTherapy!)),
+        PicosBody(child: Text(_medicationAndTherapy!)),
+        PicosBody(child: Text(_medicationAndTherapy!)),
+        PicosBody(child: Cover(title: _ready!)),
+      ];
+    }
+
+    return PicosScreenFrame(
+      title: _myEntries,
       body: PageView(
-        scrollDirection: Axis.horizontal,
-        children: const <QuestionaireCard>[
-          QuestionaireCard(
-            question: 'HAI',
-            answers: <int, String>{1: 'hai', 2: 'asdf'},
-          ),
-          QuestionaireCard(
-            question: 'HAI',
-            answers: <int, String>{1: 'hai', 2: 'asdf'},
-          ),
-          QuestionaireCard(
-            question: 'HAI',
-            answers: <int, String>{1: 'hai', 2: 'asdf'},
-          ),
-          QuestionaireCard(
-            question: 'HAI',
-            answers: <int, String>{1: 'hai', 2: 'asdf'},
-          ),
-          QuestionaireCard(
-            question: 'HAI',
-            answers: <int, String>{1: 'hai', 2: 'asdf'},
-          ),
-        ],
+        children: _pageViews!,
       ),
     );
   }
