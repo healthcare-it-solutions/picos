82912ab3db0bc2bd098d2c7f34f54ac832ee87ba
Daniel Matus <daniel.matus@hit-solutions.de>
2022-10-27 16:34:24 +0200

    Added radio selects to questionaire_screen.dart

--- lib/screens/questionaire_screen/questionaire_screen.dart
+++ lib/screens/questionaire_screen/questionaire_screen.dart
@@ -20,13 +20,12 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:picos/screens/questionaire_screen/widgets/cover.dart';
 import 'package:picos/screens/questionaire_screen/widgets/questionaire_card.dart';
+import 'package:picos/screens/questionaire_screen/widgets/radio_select_card.dart';
 import 'package:picos/screens/questionaire_screen/widgets/text_field_card.dart';
 import 'package:picos/widgets/picos_body.dart';
 import 'package:picos/widgets/picos_screen_frame.dart';
 import 'package:picos/widgets/picos_text_field.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import '../../widgets/picos_label.dart';
 
 /// This is the screen a user should see when prompted to provide some
 /// information about their health status.
@@ -49,6 +48,31 @@ class QuestionaireScreen extends StatelessWidget {
   static String? _possibleWalkDistance;
   static String? _sleepDuration;
   static String? _hrs;
+  static String? _sleepQuality7Days;
+  static String? _pain;
+  static String? _howOftenAffected;
+  static String? _lowInterest;
+  static String? _dejection;
+  static String? _nervousness;
+  static String? _controlWorries;
+  static String? _changedMedication;
+  static String? _changedTherapy;
+  static const Map<String, dynamic> _sleepQualityValues = <String, dynamic>{
+    '10': 10,
+    '9': 9,
+    '8': 8,
+    '7': 7,
+    '6': 6,
+    '5': 5,
+    '4': 4,
+    '3': 3,
+    '2': 2,
+    '1': 1,
+    '0': 0,
+  };
+  static Map<String, dynamic>? _painValues;
+  static Map<String, dynamic>? _bodyAndMindValues;
+  static Map<String, dynamic>? _medicationAndTherapyValues;
 
   static List<PicosBody>? _pageViews;
 
@@ -71,19 +95,53 @@ class QuestionaireScreen extends StatelessWidget {
       _sleepDuration = AppLocalizations.of(context)!.sleepDuration;
       _hrs = AppLocalizations.of(context)!.hrs;
       _ready = AppLocalizations.of(context)!.questionnaireFinished;
+      _sleepQuality7Days = AppLocalizations.of(context)!.sleepQuality7Days;
+      _pain = AppLocalizations.of(context)!.pain;
+      _howOftenAffected = AppLocalizations.of(context)!.howOftenAffected;
+      _lowInterest = AppLocalizations.of(context)!.lowInterest;
+      _dejection = AppLocalizations.of(context)!.dejection;
+      _nervousness = AppLocalizations.of(context)!.nervousness;
+      _controlWorries = AppLocalizations.of(context)!.controlWorries;
+      _changedTherapy = AppLocalizations.of(context)!.changedTherapy;
+      _changedMedication = AppLocalizations.of(context)!.changedMedication;
+      _painValues = <String, dynamic>{
+        '0 ${AppLocalizations.of(context)!.painless}': 0,
+        '1 ${AppLocalizations.of(context)!.veryMild}': 1,
+        '2 ${AppLocalizations.of(context)!.unpleasant}': 2,
+        '3 ${AppLocalizations.of(context)!.tolerable}': 3,
+        '4 ${AppLocalizations.of(context)!.disturbing}': 4,
+        '5 ${AppLocalizations.of(context)!.veryDisturbing}': 5,
+        '6 ${AppLocalizations.of(context)!.severe}': 6,
+        '7 ${AppLocalizations.of(context)!.verySevere}': 7,
+        '8 ${AppLocalizations.of(context)!.veryTerrible}': 8,
+        '9 ${AppLocalizations.of(context)!.agonizingUnbearable}': 9,
+        '10 ${AppLocalizations.of(context)!.strongestImaginable}': 10,
+      };
+      _bodyAndMindValues = <String, dynamic>{
+        AppLocalizations.of(context)!.notAtAll: 0,
+        AppLocalizations.of(context)!.onIndividualDays: 1,
+        AppLocalizations.of(context)!.onMoreThanHalfDays: 2,
+        AppLocalizations.of(context)!.almostEveryDays: 3,
+      };
+      _medicationAndTherapyValues = <String, dynamic>{
+        AppLocalizations.of(context)!.yes: true,
+        AppLocalizations.of(context)!.no: false,
+      };
 
       _pageViews = <PicosBody>[
         PicosBody(child: Cover(title: _vitalValues!)),
         PicosBody(
-          child: TextFieldCard(
-            label: _bodyWeight!,
-            hint: 'kg',
-          ),
-        ),
-        PicosBody(
-          child: TextFieldCard(
-            label: 'BMI',
-            hint: 'kg/m² ${_autoCalc!}',
+          child: Column(
+            children: <TextFieldCard>[
+              TextFieldCard(
+                label: _bodyWeight!,
+                hint: 'kg',
+              ),
+              TextFieldCard(
+                label: 'BMI',
+                hint: 'kg/m² ${_autoCalc!}',
+              ),
+            ],
           ),
         ),
         PicosBody(
@@ -94,30 +152,26 @@ class QuestionaireScreen extends StatelessWidget {
         ),
         PicosBody(
           child: QuestionaireCard(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                PicosLabel(label: _bloodPressure!),
-                const SizedBox(height: 15),
-                Row(
-                  children: const <Widget>[
-                    Expanded(
-                      child: PicosTextField(
-                        maxLength: 3,
-                        keyboardType: TextInputType.number,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 20, left: 10, right: 10),
-                      child: Text('/', style: TextStyle(fontSize: 20)),
-                    ),
-                    Expanded(
-                      child: PicosTextField(
-                        maxLength: 3,
-                        keyboardType: TextInputType.number,
-                      ),
-                    ),
-                  ],
+            label: _bloodPressure!,
+            child: Row(
+              children: const <Widget>[
+                Expanded(
+                  child: PicosTextField(
+                    hint: 'Syst',
+                    maxLength: 3,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20, left: 10, right: 10),
+                  child: Text('/', style: TextStyle(fontSize: 20)),
+                ),
+                Expanded(
+                  child: PicosTextField(
+                    hint: 'Dias',
+                    maxLength: 3,
+                    keyboardType: TextInputType.number,
+                  ),
                 ),
               ],
             ),
@@ -142,17 +196,68 @@ class QuestionaireScreen extends StatelessWidget {
             hint: _hrs!,
           ),
         ),
-        PicosBody(child: Text(_activityAndRest!)),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _sleepQuality7Days!,
+            options: _sleepQualityValues,
+          ),
+        ),
         PicosBody(child: Cover(title: _bodyAndMind!)),
-        PicosBody(child: Text(_bodyAndMind!)),
-        PicosBody(child: Text(_bodyAndMind!)),
-        PicosBody(child: Text(_bodyAndMind!)),
-        PicosBody(child: Text(_bodyAndMind!)),
-        PicosBody(child: Text(_bodyAndMind!)),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _pain!,
+            options: _painValues!,
+          ),
+        ),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _howOftenAffected!,
+            description: _lowInterest!,
+            options: _bodyAndMindValues!,
+          ),
+        ),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _howOftenAffected!,
+            description: _dejection!,
+            options: _bodyAndMindValues!,
+          ),
+        ),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _howOftenAffected!,
+            description: _nervousness!,
+            options: _bodyAndMindValues!,
+          ),
+        ),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _howOftenAffected!,
+            description: _controlWorries!,
+            options: _bodyAndMindValues!,
+          ),
+        ),
         PicosBody(child: Cover(title: _medicationAndTherapy!)),
-        PicosBody(child: Text(_medicationAndTherapy!)),
-        PicosBody(child: Text(_medicationAndTherapy!)),
-        PicosBody(child: Text(_medicationAndTherapy!)),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _changedMedication!,
+            options: _medicationAndTherapyValues!,
+          ),
+        ),
+        PicosBody(
+          child: RadioSelectCard(
+            callBackFunction: (dynamic value) {},
+            label: _changedTherapy!,
+            options: _medicationAndTherapyValues!,
+          ),
+        ),
         PicosBody(child: Cover(title: _ready!)),
       ];
     }
