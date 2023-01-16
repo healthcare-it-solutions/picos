162c65d6fdbe31b2e9d926be966a0db633fb18b3
mustiwebp <38396131+mustiwebp@users.noreply.github.com>
2023-01-16 12:30:50 +0100

    Background and icon added

--- lib/screens/questionaire_screen/widgets/cover.dart
+++ lib/screens/questionaire_screen/widgets/cover.dart
@@ -20,8 +20,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../themes/global_theme.dart';
-import '../../../widgets/picos_add_button_bar.dart';
-import '../../../widgets/picos_body.dart';
 import '../../../widgets/picos_ink_well_button.dart';
 
 /// A cover for the different sections of the questionaire.
@@ -36,37 +34,55 @@ class Cover extends StatelessWidget {
 
   /// The title cover.
   final String title;
+
   /// Function for getting a page back.
   final void Function()? backFunction;
+
   /// Function for getting the next page.
   final void Function()? nextFunction;
 
   @override
   Widget build(BuildContext context) {
-    final String back = AppLocalizations.of(context)!.back;
-    final String next = AppLocalizations.of(context)!.next;
+    //final String back = AppLocalizations.of(context)!.back;
+    //final String next = AppLocalizations.of(context)!.next;
+    final String start = AppLocalizations.of(context)!.letsStart;
     final GlobalTheme theme = Theme.of(context).extension<GlobalTheme>()!;
     final double height = MediaQuery.of(context).size.height;
 
-    return PicosBody(
+    return Container(
+      color: theme.darkGreen1!,
       child: Column(
         children: <Widget>[
           SizedBox(
-            height: height / 3,
+            height: height / 5,
+          ),
+          const Center(
+            child: Image(
+              image: AssetImage('assets/Vitalwerte_neg.png'),
+            ),
           ),
           Center(
-            child: Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 40,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
           SizedBox(
             height: height / 3.5,
           ),
-          PicosAddButtonBar(
+          PicosInkWellButton(
+            text: start,
+            onTap: () => nextFunction ?? () {},
+          )
+          /*PicosAddButtonBar(
             shadows: false,
             leftButton: PicosInkWellButton(
               padding: const EdgeInsets.only(
@@ -90,7 +106,7 @@ class Cover extends StatelessWidget {
               text: next,
               onTap: nextFunction ?? () {},
             ),
-          ),
+          ),*/
         ],
       ),
     );
