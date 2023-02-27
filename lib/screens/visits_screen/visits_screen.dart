1669e47e08c06f528607d646126a61852a2e7fb8
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-02-27 16:22:51 +0100

    Added info card to visits_screen.dart

--- lib/screens/visits_screen/visits_screen.dart
+++ lib/screens/visits_screen/visits_screen.dart
@@ -16,16 +16,80 @@
 */
 
 import 'package:flutter/material.dart';
+import 'package:picos/widgets/picos_display_card.dart';
+import 'package:picos/widgets/picos_info_card.dart';
+import 'package:picos/widgets/picos_page_view_item.dart';
 import 'package:picos/widgets/picos_screen_frame.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:picos/widgets/picos_text_field.dart';
+
+import '../../util/page_view_navigation.dart';
 
 /// Displays a form for filling in hospital and doctors visits.
-class VisitsScreen extends StatelessWidget {
+class VisitsScreen extends StatelessWidget with PageViewNavigation {
   /// Creates StaysScreen
-  const VisitsScreen({Key? key}) : super(key: key);
+  VisitsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const PicosScreenFrame(
+    buildContext = context;
+
+    pages.add(
+      PicosPageViewItem(
+        leftFunction: previousPage,
+        rightFunction: nextPage,
+        child: Column(
+          children: <Widget>[
+            PicosInfoCard(
+              infoText: RichText(
+                text: TextSpan(
+                  text: AppLocalizations.of(context)!.visitInfoPart1,
+                  style: const TextStyle(
+                    color: PicosInfoCard.infoTextFontColor,
+                    fontSize: PicosInfoCard.infoTextFontSize,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: AppLocalizations.of(context)!.visitInfoPart2,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: AppLocalizations.of(context)!.visitInfoPart3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            PicosDisplayCard(
+              child: const PicosTextField(),
+            ),
+          ],
+        ),
+      ),
+    );
+    pages.add(PicosPageViewItem(
+      leftFunction: previousPage,
+      rightFunction: nextPage,
+      child: const Text('fdas'),
+    ),);
+    pages.add(
+      PicosPageViewItem(
+        leftFunction: previousPage,
+        rightFunction: nextPage,
+        rightButtonTitle: AppLocalizations.of(context)!.save,
+        child: const Text('hjmgfjkrz'),
+      ),
+    );
+
+    return PicosScreenFrame(
+      title: AppLocalizations.of(context)!.visits,
+      body: PageView(
+        controller: controller,
+        physics: const NeverScrollableScrollPhysics(),
+        children: pages,
+      ),
     );
   }
 }
