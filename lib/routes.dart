834335db084b6aebc71a71c58f7d057976cc8d2e
mustiwebp <38396131+mustiwebp@users.noreply.github.com>
2023-03-13 10:48:07 +0100

    Some adjustments

--- lib/routes.dart
+++ lib/routes.dart
@@ -24,6 +24,7 @@ import 'package:picos/screens/physician_list_screen/add_physician_screen.dart';
 import 'package:picos/screens/family_member_list_screen/add_family_member_screen.dart';
 import 'package:picos/screens/family_member_list_screen/family_members_screen.dart';
 import 'package:picos/screens/privacy_notice_screen.dart';
+import 'package:picos/screens/study_nurse_screen/menu_screen/add_patient_screen.dart';
 import 'package:picos/screens/visits_screen/visits_screen.dart';
 import 'package:picos/screens/study_nurse_screen/configuration_screen/configuration_finished_screen.dart';
 import 'package:picos/screens/study_nurse_screen/configuration_screen/configuration_pages.dart';
@@ -72,8 +73,11 @@ class Routes {
       '/my-therapy-screen/my-therapy': (BuildContext ctx) =>
           const MyTherapiesScreen(),
       '/legals-screen': (BuildContext ctx) => const LegalsScreen(),
-      '/privacy-notice-screen': (BuildContext ctx) => const PrivacyNoticeScreen(),
+      '/privacy-notice-screen': (BuildContext ctx) =>
+          const PrivacyNoticeScreen(),
       '/visits-screen/visits': (BuildContext ctx) => const VisitsScreen(),
+      'study-nurse-screen/menu-screen/add-patient': (BuildContext ctx) =>
+          const AddPatientScreen(),
     };
   }
 
--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -20,6 +20,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:picos/api/backend_medications_api.dart';
+import 'package:picos/api/backend_patient_api.dart';
 import 'package:picos/api/backend_stays_api.dart';
 import 'package:picos/api/backend_therapies_api.dart';
 import 'package:picos/api/database_object_api.dart';
@@ -44,6 +45,12 @@ class MainScreen extends StatelessWidget {
           create: (BuildContext context) =>
               ObjectsListBloc<BackendMedicationsApi>(
             BackendMedicationsApi(),
+          )..add(const ObjectsListSubscriptionRequested()),
+        ),
+        BlocProvider<ObjectsListBloc<BackendPatientApi>>(
+          create: (BuildContext context) =>
+              ObjectsListBloc<BackendPatientApi>(
+            BackendPatientApi(),
           )..add(const ObjectsListSubscriptionRequested()),
         ),
         BlocProvider<ObjectsListBloc<BackendTherapiesApi>>(
