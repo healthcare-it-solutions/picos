ce15dd91241a9443788b56df388bfaf54e560edd
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-03-27 12:46:17 +0200

    Added physician_card.dart

---
+++ lib/screens/physician_list_screen/widgets/physician_card.dart
@@ -0,0 +1,64 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:picos/api/backend_physicians_api.dart';
+
+import '../../../models/physician.dart';
+import '../../../state/objects_list_bloc.dart';
+import '../../../widgets/picos_list_card.dart';
+
+/// The card displaying physician information.
+class PhysicianCard extends StatelessWidget {
+  /// Creates PhysicianCard.
+  const PhysicianCard(this._physician, {Key? key}) : super(key: key);
+
+  final Physician _physician;
+
+  @override
+  Widget build(BuildContext context) {
+    return PicosListCard(
+      edit: () {
+        Navigator.of(context).pushNamed(
+          '/physician-list-screen/add-physician',
+          arguments: _physician,
+        );
+      },
+      delete: () {
+        context
+            .read<ObjectsListBloc<BackendPhysiciansApi>>()
+            .add(RemoveObject(_physician));
+      },
+      title: _physician.subjectArea,
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(_physician.form),
+            Text(_physician.name),
+            Text('${_physician.address} ${_physician.city}'),
+            Text(_physician.phone),
+            Text(_physician.mail),
+            Text(_physician.homepage),
+          ],
+        ),
+      ),
+    );
+  }
+}
