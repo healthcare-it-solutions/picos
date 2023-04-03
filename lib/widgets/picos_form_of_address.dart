a10b72563f073f44c2357c8316dfa2a8f656a265
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-04-03 11:28:03 +0200

    Created new picos_form_of_address.dart widget.

---
+++ lib/widgets/picos_form_of_address.dart
@@ -0,0 +1,153 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+/// Creates a horizontal radio selection for
+class PicosFormOfAddress extends StatefulWidget {
+  /// PicosFormOfAddress constructor.
+  const PicosFormOfAddress({
+    required this.callBackFunction,
+    Key? key,
+  }) : super(key: key);
+
+  /// The function that is executed when an item gets selected.
+  final Function(String value) callBackFunction;
+
+  @override
+  State<PicosFormOfAddress> createState() => _PicosFormOfAddressState();
+}
+
+/// Enumeration for FormOfAddress-variable.
+enum FormOfAddress {
+  /// Element for denoting 'male'.
+  male,
+
+  /// Element for denoting 'female'.
+  female,
+
+  /// Element for denoting 'diverse'.
+  diverse,
+}
+
+class _PicosFormOfAddressState extends State<PicosFormOfAddress> {
+  static String? _mrs;
+  static String? _mr;
+  static String? _diverse;
+
+  FormOfAddress? _groupValue;
+
+  void _updateState(FormOfAddress value) {
+    widget.callBackFunction(value.name);
+
+    setState(() {
+      _groupValue = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double fontSize = 18;
+    const double paddingBetween = 5;
+    const double paddingEnd = 10;
+
+    if (_mrs == null) {
+      _mrs = AppLocalizations.of(context)!.mrs;
+      _mr = AppLocalizations.of(context)!.mr;
+      _diverse = AppLocalizations.of(context)!.diverse;
+    }
+
+    return Row(
+      children: <InkWell>[
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              const SizedBox(width: paddingBetween),
+              Radio<FormOfAddress>(
+                visualDensity: VisualDensity(
+                  horizontal: VisualDensity.minimumDensity,
+                  vertical: VisualDensity.standard.vertical,
+                ),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                value: FormOfAddress.female,
+                groupValue: _groupValue,
+                onChanged: (FormOfAddress? value) {
+                  _updateState(value!);
+                },
+              ),
+              const SizedBox(width: paddingBetween),
+              Text(_mrs!, style: const TextStyle(fontSize: fontSize)),
+              const SizedBox(width: paddingEnd),
+            ],
+          ),
+          onTap: () {
+            _updateState(FormOfAddress.female);
+          },
+        ),
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              const SizedBox(width: paddingBetween),
+              Radio<FormOfAddress>(
+                visualDensity: VisualDensity(
+                  horizontal: VisualDensity.minimumDensity,
+                  vertical: VisualDensity.standard.vertical,
+                ),
+                value: FormOfAddress.male,
+                groupValue: _groupValue,
+                onChanged: (FormOfAddress? value) {
+                  _updateState(value!);
+                },
+              ),
+              const SizedBox(width: paddingBetween),
+              Text(_mr!, style: const TextStyle(fontSize: fontSize)),
+              const SizedBox(width: paddingEnd),
+            ],
+          ),
+          onTap: () {
+            _updateState(FormOfAddress.male);
+          },
+        ),
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              const SizedBox(width: paddingBetween),
+              Radio<FormOfAddress>(
+                visualDensity: VisualDensity(
+                  horizontal: VisualDensity.minimumDensity,
+                  vertical: VisualDensity.standard.vertical,
+                ),
+                value: FormOfAddress.diverse,
+                groupValue: _groupValue,
+                onChanged: (FormOfAddress? value) {
+                  _updateState(value!);
+                },
+              ),
+              const SizedBox(width: paddingBetween),
+              Text(_diverse!, style: const TextStyle(fontSize: fontSize)),
+              const SizedBox(width: paddingEnd),
+            ],
+          ),
+          onTap: () {
+            _updateState(FormOfAddress.diverse);
+          },
+        ),
+      ],
+    );
+  }
+}
