efc30efaf616627be146c57d6a30a3615934d34b
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-04-20 11:12:15 +0200

    Styling documents_list.dart

--- lib/screens/my_documents_screen/documents_list.dart
+++ lib/screens/my_documents_screen/documents_list.dart
@@ -17,10 +17,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:picos/models/abstract_database_object.dart';
+import 'package:picos/widgets/picos_label.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import '../../api/backend_documents_api.dart';
 import '../../models/document.dart';
 import '../../state/objects_list_bloc.dart';
 import 'document_item.dart';
+
 /// A List with all documents.
 class DocumentsList extends StatefulWidget {
   /// Creates DocumentsList.
@@ -31,8 +36,55 @@ class DocumentsList extends StatefulWidget {
 }
 
 class _DocumentsListState extends State<DocumentsList> {
+  static String? _importantDocuments;
+  static String? _documentsSortedDesc;
+
+  List<Widget>? _importantList;
+  List<Widget>? _sortedList;
+
+  void _createLists(List<AbstractDatabaseObject> list) {
+    const Padding separator = Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Divider(
+        thickness: 1,
+        height: 0,
+      ),
+    );
+
+    _importantList = <Widget>[];
+    _sortedList = <Widget>[];
+
+    list = _sortList(list);
+
+    for (AbstractDatabaseObject element in list) {
+      DocumentItem documentItem = DocumentItem(element as Document);
+
+      if (element.important) {
+        _importantList!.add(documentItem);
+        _importantList!.add(separator);
+        continue;
+      }
+
+      _sortedList!.add(documentItem);
+      _sortedList!.add(separator);
+    }
+  }
+
+  List<AbstractDatabaseObject> _sortList(List<AbstractDatabaseObject> list) {
+    list.sort((AbstractDatabaseObject a, AbstractDatabaseObject b) {
+      return (b as Document).date.compareTo((a as Document).date);
+    });
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_importantDocuments == null) {
+      _importantDocuments = AppLocalizations.of(context)!.importantDocuments;
+      _documentsSortedDesc = AppLocalizations.of(context)!.documentsSortedDesc;
+    }
+
     return BlocBuilder<ObjectsListBloc<BackendDocumentsApi>, ObjectsListState>(
       builder: (BuildContext context, ObjectsListState state) {
         if (state.objectsList.isEmpty &&
@@ -48,20 +100,25 @@ class _DocumentsListState extends State<DocumentsList> {
           );
         }
 
-        return ListView.separated(
-          itemCount: state.objectsList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return DocumentItem(state.objectsList[index] as Document);
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Divider(
-                thickness: 1,
-                height: 0,
-              ),
-            );
-          },
+        _createLists(state.objectsList);
+
+        return Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 10),
+              child: PicosLabel(_importantDocuments!),
+            ),
+            Column(
+              children: _importantList!,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 50),
+              child: PicosLabel(_documentsSortedDesc!),
+            ),
+            Column(
+              children: _sortedList!,
+            ),
+          ],
         );
       },
     );
