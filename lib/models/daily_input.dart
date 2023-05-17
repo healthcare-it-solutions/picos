f30f18b2c41d0b0f79aa6d55853e64de68508ef2
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-05-17 11:00:57 +0200

    Changes to daily_input.dart model

--- lib/models/daily_input.dart
+++ lib/models/daily_input.dart
@@ -27,7 +27,7 @@ class DailyInput extends AbstractDatabaseObject {
   /// Creates a Daily object.
   const DailyInput({
     required this.day,
-    this.daily,
+    required this.daily,
     this.weekly,
     this.phq4,
     String? objectId,
@@ -42,7 +42,7 @@ class DailyInput extends AbstractDatabaseObject {
   final int day;
 
   /// [Daily]
-  final Daily? daily;
+  final Daily daily;
 
   /// [Weekly]
   final Weekly? weekly;
@@ -77,7 +77,9 @@ class DailyInput extends AbstractDatabaseObject {
   }
 
   @override
-  List<Object> get props => <Object>[];
+  List<Object> get props => <Object>[
+    daily,
+  ];
 
   @override
   Map<String, dynamic> get databaseMapping => <String, dynamic>{};
