3f12bb109cc2c875c5fd67792e615e3b5932553f
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-06-23 16:00:46 +0200

    Creates new picos_number_field.dart widget

---
+++ lib/widgets/picos_number_field.dart
@@ -0,0 +1,65 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:picos/widgets/picos_text_field.dart';
+
+/// Creates a text field for entering a number.
+class PicosNumberField extends StatelessWidget {
+  /// Creates PicosNumberField.
+  const PicosNumberField({
+    this.hint = '',
+    Key? key,
+    this.initialValue,
+    this.onChanged,
+    this.digitsOnly = false,
+  }) : super(key: key);
+
+  /// Initial value.
+  final String? initialValue;
+
+  /// Hint shown when empty.
+  final String hint;
+
+  /// Callback function when the value changes.
+  final dynamic Function(String)? onChanged;
+
+  /// If true prevents [double]. Only [int].
+  final bool digitsOnly;
+
+  @override
+  Widget build(BuildContext context) {
+    List<TextInputFormatter> inputFormatter = <TextInputFormatter>[];
+
+    if (digitsOnly) {
+      inputFormatter = <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ];
+    }
+
+    return PicosTextField(
+      onChanged: onChanged,
+      initialValue: initialValue,
+      hint: hint,
+      inputFormatters: inputFormatter,
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: true,
+      ),
+    );
+  }
+}
