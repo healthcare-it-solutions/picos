5aa7a4b23bcc37905c2f68873749146bad3ca964
S1rDan <99737773+S1rDan@users.noreply.github.com>
2023-06-27 12:41:55 +0200

    Added font weight attribute for catalog_of_items_label.dart

--- lib/screens/study_nurse_screen/catalog_of_items_screen/widgets/catalog_of_items_label.dart
+++ lib/screens/study_nurse_screen/catalog_of_items_screen/widgets/catalog_of_items_label.dart
@@ -4,13 +4,24 @@ import 'package:picos/widgets/picos_label.dart';
 /// Reusable label for the catalog of items.
 class CatalogOfItemsLabel extends StatelessWidget {
   /// Creates CatalogOfItemsLabel
-  const CatalogOfItemsLabel(this.label, {Key? key}) : super(key: key);
+  const CatalogOfItemsLabel(
+    this.label, {
+    Key? key,
+    this.fontWeight = FontWeight.bold,
+  }) : super(key: key);
 
   /// The label to be shown.
   final String label;
 
+  /// Custom font weight.
+  final FontWeight fontWeight;
+
   @override
   Widget build(BuildContext context) {
-    return PicosLabel(label, fontSize: 15,);
+    return PicosLabel(
+      label,
+      fontSize: 15,
+      fontWeight: fontWeight,
+    );
   }
 }
