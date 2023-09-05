e517ddfe2c39dbad729b7dc6ac8908a55b2023f6
AzureAD\FaysalQurabi <faysal.qurabi@gmail.com>
2023-09-05 15:38:14 +0200

    Add picos_chart_frame.dart and myValues.dart Adjust graph_section.dart and backend_values_api.dart

--- lib/api/backend_values_api.dart
+++ lib/api/backend_values_api.dart
@@ -1,79 +1,86 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:flutter/foundation.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:picos/models/myValues.dart';
 
 import '../models/daily.dart';
-import '../models/daily_input.dart';
 import '../models/weekly.dart';
-import '../util/backend.dart';
 
 /// API for storing values at the backend.
 class BackendValuesApi {
-  ///
-  static bool checkDay(dynamic value, String key) {
-    if (value[key] != null) {
-      return true;
-    }
-
-    return false;
-  }
-
   /// Ret
-  static Future<List<DailyInput>?> getMyValues() async {
+  static Future<MyValues?> getMyValues() async {
     try {
-      Map<String, dynamic> response = (await Backend.callEndpoint(
-        'getPatientsDailyInput',
-        <String, int>{'days': 90},
-      ))
-          .first['result'];
+      DateTime now = DateTime.now();
 
-      List<DailyInput> results = <DailyInput>[];
-      int day = 0;
+      //Weekly.
+      DateTime last7Weeks = now.subtract(const Duration(days: 49));
 
-      response.forEach((String key, dynamic value) {
-        Daily? daily;
-        Weekly? weekly;
-        bool weeklyDay = checkDay(value, 'weekly');
-
-        if (value['daily']['objectId'] != null) {
-          daily = Daily(
-            date: DateTime.parse(value['daily']['datetime']['iso']),
-            heartFrequency: value['daily']['HeartRate'],
-            bloodSugar: value['daily']['BloodSugar'],
-            bloodSystolic: value['daily']['BloodPSystolic'],
-            bloodDiastolic: value['daily']['BloodPDiastolic'],
-            sleepDuration: value['daily']['SleepDuration']?.toDouble(),
-            pain: value['daily']['Pain'],
-            bloodSugarMol: value['daily']['BloodSugarMol']?.toDouble(),
-            objectId: value['daily']['objectId'],
-            createdAt: DateTime.parse(value['daily']['createdAt']),
-            updatedAt: DateTime.parse(value['daily']['updatedAt']),
-          );
-        }
-
-        if (weeklyDay && value['weekly']?['objectId'] != null) {
-          weekly = Weekly(
-            date: DateTime.parse(value['weekly']['datetime']['iso']),
-            bmi: value['weekly']['BMI']?.toDouble(),
-            bodyWeight: value['weekly']['BodyWeight']?.toDouble(),
-            sleepQuality: value['weekly']['SISQS'],
-            walkingDistance: value['weekly']['WalkingDistance'],
-            objectId: value['weekly']['objectId'],
-            createdAt: DateTime.parse(value['weekly']['createdAt']),
-            updatedAt: DateTime.parse(value['weekly']['updatedAt']),
-          );
-        }
-
-        results.add(
-          DailyInput(
-            day: day,
-            daily: daily,
-            weekly: weekly,
-            weeklyDay: weeklyDay,
+      QueryBuilder<ParseObject> weeklyQueryBuilder =
+          QueryBuilder<ParseObject>(ParseObject(Weekly.databaseTable));
+      weeklyQueryBuilder.whereGreaterThanOrEqualsTo('datetime', last7Weeks);
+      ParseResponse responseLast7Weeks = await weeklyQueryBuilder.query();
+      List<Weekly> resultsLast7Weeks = <Weekly>[];
+      for (dynamic element in responseLast7Weeks.results!) {
+        resultsLast7Weeks.add(
+          Weekly(
+            date: element['datetime'],
+            bmi: element['BMI']?.toDouble(),
+            bodyWeight: element['BodyWeight']?.toDouble(),
+            sleepQuality: element['SISQS'],
+            walkingDistance: element['WalkingDistance'],
+            objectId: element['objectId'],
+            createdAt: element['createdAt'],
+            updatedAt: element['updatedAt'],
           ),
         );
+      }
 
-        day++;
-      });
-      return results;
+      //Daily.
+      DateTime last7Days = now.subtract(const Duration(days: 7));
+      QueryBuilder<ParseObject> dailyQueryBuilder =
+          QueryBuilder<ParseObject>(ParseObject(Daily.databaseTable));
+      dailyQueryBuilder.whereGreaterThanOrEqualsTo('datetime', last7Days);
+      dynamic responseLast7Days = await dailyQueryBuilder.query();
+      List<Daily> resultsLast7Days = <Daily>[];
+      for (dynamic element in responseLast7Days.results!) {
+        resultsLast7Days.add(
+          Daily(
+            date: element['datetime'],
+            heartFrequency: element['HeartRate'],
+            bloodSugar: element['BloodSugar'],
+            bloodSystolic: element['BloodPSystolic'],
+            bloodDiastolic: element['BloodPDiastolic'],
+            sleepDuration: element['SleepDuration']?.toDouble(),
+            pain: element['Pain'],
+            bloodSugarMol: element['BloodSugarMol']?.toDouble(),
+            objectId: element['objectId'],
+            createdAt: element['createdAt'],
+            updatedAt: element['updatedAt'],
+          ),
+        );
+      }
+
+      return MyValues(
+        dailyList: resultsLast7Days,
+        weeklyList: resultsLast7Weeks,
+      );
     } catch (e) {
       if (kDebugMode) {
         print(e);
---
+++ lib/widgets/picos_chart_frame.dart
@@ -0,0 +1,85 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import '../screens/home_screen/overview/widgets/graph_section.dart';
+import '../themes/global_theme.dart';
+
+///
+class PicosChartFrame extends StatelessWidget {
+  /// Creates the PicosBody.
+  const PicosChartFrame({required this.chart, this.title, Key? key})
+      : super(key: key);
+
+  /// Creates a child Widget inside the PicosBody.
+  final Widget chart;
+
+  ///
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalTheme theme = Theme.of(context).extension<GlobalTheme>()!;
+    return Stack(
+      children: <Widget>[
+        chart,
+        Positioned.fill(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Transform.scale(
+              scaleX: 1.1,
+              scaleY: 1.5,
+              child: Container(
+                decoration: BoxDecoration(
+                  border:
+                      Border.all(color: theme.blue!, width: ChartHelper.width),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 20, // Adjust as needed
+          left: 0,
+          right: 225,
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: theme.blue!, // Farbe des Unterstrichs
+                    width: ChartHelper.width, // Dicke des Unterstrichs
+                  ),
+                ),
+              ),
+              padding: const EdgeInsets.only(bottom: 1.0), // Abstand dazwischen
+              child: Text(
+                title ?? '',
+                style: TextStyle(
+                  color: theme.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
