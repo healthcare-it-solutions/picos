7824f06be73637d4156ede2c575254df567c5557
AzureAD\FaysalQurabi <faysal.qurabi@gmail.com>
2023-09-21 12:11:03 +0200

    Bugfix (Exception caught by widgets library)

--- lib/widgets/picos_select.dart
+++ lib/widgets/picos_select.dart
@@ -109,32 +109,30 @@ class _PicosSelectState extends State<PicosSelect> {
           alignedDropdown: true,
           child: SizedBox(
             height: widget.validator == null ? null : 75,
-            child: Expanded(
-              child: DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.only(
-                    right: 5,
-                  ),
-                  enabledBorder: outlineInputBorder,
-                  focusedBorder: outlineInputBorder,
-                  errorBorder: errorInputBorder,
-                  focusedErrorBorder: errorInputBorder,
+            child: DropdownButtonFormField<String>(
+              isExpanded: true,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(
+                  right: 5,
                 ),
-                borderRadius: borderRadius,
-                value: _dropdownValue,
-                icon: const Icon(Icons.keyboard_arrow_down),
-                hint: Text(widget.hint ?? ''),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    final String value = newValue ?? '';
-
-                    widget.callBackFunction(value);
-                    _dropdownValue = value;
-                  });
-                },
-                items: _createItemList(),
-                validator: widget.validator,
+                enabledBorder: outlineInputBorder,
+                focusedBorder: outlineInputBorder,
+                errorBorder: errorInputBorder,
+                focusedErrorBorder: errorInputBorder,
               ),
+              borderRadius: borderRadius,
+              value: _dropdownValue,
+              icon: const Icon(Icons.keyboard_arrow_down),
+              hint: Text(widget.hint ?? ''),
+              onChanged: (String? newValue) {
+                setState(() {
+                  final String value = newValue ?? '';
+                  widget.callBackFunction(value);
+                  _dropdownValue = value;
+                });
+              },
+              items: _createItemList(),
+              validator: widget.validator,
             ),
           ),
         ),
