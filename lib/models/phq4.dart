b79543d34d1eebc3f8dd37247a2a7a4b83b9ef87
mustiwebp <38396131+mustiwebp@users.noreply.github.com>
2023-11-20 20:21:39 +0100

    Progress lights bug fixed (#220)
    * added count-function
    
    * initial work on displaying progress
    
    * bugfix
    
    * Problem solved
    
    * Localizations
    
    * CircularProgressIndicator added
    
    * Localizations adjusted

--- lib/models/phq4.dart
+++ lib/models/phq4.dart
@@ -20,7 +20,7 @@ import 'package:picos/models/abstract_database_object.dart';
 /// Class with PHQ4 answers.
 class PHQ4 extends AbstractDatabaseObject {
   /// Creates a PHQ4 object.
-  PHQ4({
+  const PHQ4({
     required this.date,
     this.a,
     this.b,
@@ -29,10 +29,7 @@ class PHQ4 extends AbstractDatabaseObject {
     String? objectId,
     DateTime? createdAt,
     DateTime? updatedAt,
-  }) : super(objectId: objectId, createdAt: createdAt, updatedAt: updatedAt) {
-    hasNullValues = _checkHasNullValues();
-    hasAnyValue = _checkHasAnyValue();
-  }
+  }) : super(objectId: objectId, createdAt: createdAt, updatedAt: updatedAt);
 
   /// The database table the objects are stored in.
   static const String databaseTable = 'PICOS_PHQ4';
@@ -51,12 +48,6 @@ class PHQ4 extends AbstractDatabaseObject {
 
   /// The assessment date.
   final DateTime date;
-
-  /// The information if this objects has any null values.
-  late final bool hasNullValues;
-
-  /// The information if this objects has any non-null value.
-  late final bool hasAnyValue;
 
   @override
   get table {
@@ -99,20 +90,4 @@ class PHQ4 extends AbstractDatabaseObject {
         'd': d,
         'datetime': date,
       };
-
-  bool _checkHasNullValues() {
-    if (a == null || b == null || c == null || d == null) {
-      return true;
-    }
-
-    return false;
-  }
-
-  bool _checkHasAnyValue() {
-    if (a != null || b != null || c != null || d != null) {
-      return true;
-    }
-
-    return false;
-  }
 }
