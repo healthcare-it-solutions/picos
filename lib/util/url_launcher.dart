ce22d5f80ca9e78c4c5a48a1a02c18499796009c
mustiwebp <38396131+mustiwebp@users.noreply.github.com>
2024-02-23 10:56:07 +0100

    Link for deleting account added & refactoring (#272)
    * Link for deleting account added & refactoring
    
    * Class for launching URLs created
    
    * Refactoring
    
    * Use of UrlLauncher-class
    
    * Use of UrlLauncher-class and formatting
    
    * Adjust color

---
+++ lib/util/url_launcher.dart
@@ -0,0 +1,43 @@
+/*   This file is part of Picos, a health tracking mobile app
+*    Copyright (C) 2022 Healthcare IT Solutions GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// This class provides a method to launch URLs.
+class UrlLauncher {
+  /// Method for launching URLs.
+  void launch(BuildContext context, String uri) async {
+    final Uri url = Uri.parse(
+      uri,
+    );
+    final ScaffoldMessengerState scaffoldMessenger =
+        ScaffoldMessenger.of(context);
+    try {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } catch (e) {
+      scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content: Text(
+            context.mounted ? AppLocalizations.of(context)!.couldNotLaunch : '',
+          ),
+        ),
+      );
+    }
+  }
+}
