6a57f8378e543a92a4e16e07e89779d69ea9e18c
FaysalSolutions <faysal.qurabi@gmail.com>
2024-06-26 12:25:37 +0200

    Reset menu_main_screen.dart

--- lib/screens/study_nurse_screen/menu_screen/menu_main_screen.dart
+++ lib/screens/study_nurse_screen/menu_screen/menu_main_screen.dart
@@ -19,46 +19,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:picos/screens/study_nurse_screen/menu_screen/patients_list.dart';
 import 'package:picos/widgets/picos_add_mono_button_bar.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../api/backend_patients_list_api.dart';
-import '../../../state/objects_list_bloc.dart';
+import 'package:picos/widgets/picos_screen_frame.dart';
 
 /// Shows a list with all patients.
-class MenuMainScreen extends StatefulWidget {
+class MenuMainScreen extends StatelessWidget {
   /// Creates MenuMainScreen.
   const MenuMainScreen({Key? key}) : super(key: key);
 
   @override
-  State<MenuMainScreen> createState() => _MenuMainScreenState();
-}
-
-class _MenuMainScreenState extends State<MenuMainScreen> {
-  @override
-  void initState() {
-    super.initState();
-    refreshPatientsList();
-  }
-
-  /// Refresh the list of patients
-  void refreshPatientsList() {
-    context.read<ObjectsListBloc<BackendPatientsListApi>>().add(
-          const LoadObjectsList(),
-        );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.myPatients)),
+    return PicosScreenFrame(
+      title: AppLocalizations.of(context)!.myPatients,
       body: const PatientsList(),
       bottomNavigationBar: const PicosAddMonoButtonBar(
         route: '/study-nurse-screen/configuration-screen',
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: refreshPatientsList,
-        tooltip: 'Refresh Patients List',
-        child: const Icon(Icons.refresh),
       ),
     );
   }
