d0bed964497bea5f67e8a93dd865ab842a3e5409
FaysalSolutions <132355030+FaysalSolutions@users.noreply.github.com>
2024-06-27 18:57:18 +0200

    Release 1.20.0 (#300)
    * Bugfix/update patient list after changing patient data (#298)
    
    * Delete static variables.
    
    * Create refresh button
    
    * Reset menu_main_screen.dart
    
    * Delete static variables
    
    * Use PatientsListElement as arguments in Navigator
    
    * Take PatientsListElement from arguments
    
    * Use _patientsListElement to update patientData
    
    * Update the UI
    
    * Adjust comment
    
    * Format the code
    
    * Format class
    
    * Adjust name of methode
    
    * Feature/refresh button patientlist (#297)
    
    * Create "appBarActions" variable
    
    * Use "appBarActions" to update the patient list
    
    * Release 1.20.0 (#299)

--- lib/screens/study_nurse_screen/catalog_of_items_screen/catalog_of_items_screen.dart
+++ lib/screens/study_nurse_screen/catalog_of_items_screen/catalog_of_items_screen.dart
@@ -18,6 +18,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:picos/api/backend_catalog_of_items_api.dart';
+import 'package:picos/api/backend_patients_list_api.dart';
+import 'package:picos/models/abstract_database_object.dart';
 import 'package:picos/models/blood_gas_analysis.dart';
 import 'package:picos/models/blood_gas_analysis_object.dart';
 import 'package:picos/models/catalog_of_items_element.dart';
@@ -35,6 +37,7 @@ import 'package:picos/widgets/picos_screen_frame.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../models/labor_parameters.dart';
+import '../../../models/patients_list_element.dart';
 import '../../../themes/global_theme.dart';
 import '../../../widgets/picos_add_button_bar.dart';
 import '../../../widgets/picos_ink_well_button.dart';
@@ -55,6 +58,7 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
 
   // All values can be accessed here.
   CatalogOfItemsPageStorage? pageStorage;
+  PatientsListElement? _patientsListElement;
   CatalogOfItemsElement? _catalogOfItemsElement;
   bool isLoading = true;
 
@@ -72,11 +76,22 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
         context.read<ObjectsListBloc<BackendCatalogOfItemsApi>>().add(
               SaveObject(catalogOfItemsElement),
             );
+        updatePatientsListElement(catalogOfItemsElement);
         Navigator.of(context).pop();
       } catch (e) {
         Stream<String>.error(e);
       }
     }
+  }
+
+  updatePatientsListElement(CatalogOfItemsElement catalogOfItemsElement) {
+    context.read<ObjectsListBloc<BackendPatientsListApi>>().add(
+          SaveObject(
+            _patientsListElement?.copyWith(
+              patientData: catalogOfItemsElement.movementData,
+            ) as AbstractDatabaseObject,
+          ),
+        );
   }
 
   CatalogOfItemsElement createCatalogFromPageStorage() {
@@ -340,7 +355,7 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
         bodyHeight: pageStorage?.bodyHeight,
         patientID: pageStorage?.patientID,
         caseNumber: pageStorage?.caseNumber,
-        instKey: EditPatientScreen.instituteKey,
+        instKey: pageStorage?.instKey,
         patientObjectId: EditPatientScreen.patientObjectId,
         doctorObjectId: Backend.user.objectId,
         bodyWeight: pageStorage?.bodyWeight,
@@ -533,7 +548,7 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
       bodyHeight: pageStorage?.bodyHeight,
       patientID: pageStorage?.patientID,
       caseNumber: pageStorage?.caseNumber,
-      instKey: EditPatientScreen.instituteKey,
+      instKey: pageStorage?.instKey,
       patientObjectId: EditPatientScreen.patientObjectId!,
       doctorObjectId: Backend.user.objectId!,
       bodyWeight: pageStorage?.bodyWeight,
@@ -574,7 +589,7 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
         pages = pageStorage!.pages;
         nextPageCallback = _nextPageCallback;
       }
-      // When the DB is empty
+      // When the DB is empty.
       else {
         pageStorage = CatalogOfItemsPageStorage(context: context);
       }
@@ -588,6 +603,8 @@ class _CatalogOfItemsScreenState extends State<CatalogOfItemsScreen>
     GlobalTheme? theme = Theme.of(context).extension<GlobalTheme>()!;
     buildContext = context;
     int lastPage = pages.length - 1;
+    _patientsListElement ??=
+        ModalRoute.of(context)!.settings.arguments as PatientsListElement;
 
     if (isLoading) {
       return const Center(child: CircularProgressIndicator());
